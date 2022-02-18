3608789dfd840128b167874464eff64babcb8a1c
Rahul E <erahul096@gmail.com>
2022-02-18 22:07:18 +0530

    fixed env port

--- app.js
+++ app.js
@@ -118,10 +118,13 @@ app
 
 
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000
+}
 
 
-
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("app started in local host 3000");
   });
   
